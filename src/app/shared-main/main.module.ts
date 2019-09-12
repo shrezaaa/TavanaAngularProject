c97bd56d276358cc3c7b8c90fa132d7f1ed915fa
reza shakeri <r.shakeri9210@gmail.com>
2019-09-12 13:16:04 +0430

    This is a new commit for what I originally planned to be amended

--- src/app/shared-main/main.module.ts
+++ src/app/shared-main/main.module.ts
@@ -2,25 +2,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
+
 
 @NgModule({
   imports:      [ CommonModule ],
   declarations: [],
   exports:      [
-    CommonModule, FormsModule ]
-=======
-@NgModule({
-  imports:      [ 
-    CommonModule 
-  ],
-  declarations: [
+    CommonModule, FormsModule ],
 
-  ],
-  exports:[
-     CommonModule,
-      FormsModule 
-    ]
->>>>>>> ffee3f74bc7e91379c0ab139095019a4529ce29e
 })
 export class MainModule { }
